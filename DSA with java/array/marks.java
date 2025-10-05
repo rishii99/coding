ce8352e7b7990626b9717be4a590574e8d91e5cc
rishii99 <rishimadhesiya9984@gmail.com>
2025-10-05 23:44:34 +0530

    df
    fged

---
+++ DSA with java/array/marks.java
@@ -0,0 +1,13 @@
+package array;
+
+public class marks {
+    public static void main(String[] args) {
+        int [] marks = {97,65,23,65,55,13};
+        for(int i =0; i<marks.length; i++){
+            if(marks[i]<35)
+          //  System.out.println(marks[i]+ " ");
+          System.out.println(i+ " ");
+        }
+    }
+}
+
