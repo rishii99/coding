54e8b8f37f71b809481d19e198d36fed5324f729
rishii99 <rishimadhesiya9984@gmail.com>
2025-10-11 23:57:33 +0530

    11/10/25
    ..

---
+++ Leetcode/TwoSum.java
@@ -0,0 +1,25 @@
+
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+
+        if (result.length > 0)
+            System.out.println("Indices: " + result[0] + " and " + result[1]);
+        else
+            System.out.println("No pair found");
+    }
+}
