452e5b4b7c1395034e88780bec651094942372c3
rishii99 <rishimadhesiya9984@gmail.com>
2025-10-13 12:31:50 +0530

    .
    .

---
+++ DSA with java/pattern/Star.java
@@ -0,0 +1,14 @@
+package pattern;
+
+public class Star {
+    public static void main(String[] args) {
+        int n = 5; // Number of rows
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
