86142af6b341eb521a75bab91b12e4c20f9aade2
rishii99 <rishimadhesiya9984@gmail.com>
2025-12-02 15:23:38 +0530

    Add tribonacci number calculator in Java
    Introduced tribonacci.java with a memoized recursive solution to compute the nth tribonacci number. Includes a main method for demonstration. Added corresponding compiled class files.

---
+++ Leetcode/tribonacci.java
@@ -0,0 +1,19 @@
+class tribonacci{
+    public int tribonacci(int n) {
+        int[]memo= new int[n+1];
+        for(int i =0;i<=n; i++)memo[i]=-1;
+        return solve(n,memo);
+    }
+    private int solve(int n, int[]memo){
+        if (n==0) return 0;
+        if (n==1 || n==2) return 1;
+        if (memo[n] != -1) return memo[n];
+        memo[n] = solve(n-1,memo)+solve(n-2,memo)+solve(n-3,memo);
+        return memo[n];
+    } 
+    public static void main(String[] args) {
+        tribonacci obj = new tribonacci();
+        System.out.println(obj.tribonacci(4));  // example: tribonacci(4)  
+    }
+}  
+     
