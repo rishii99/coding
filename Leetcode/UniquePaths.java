c0388de233f8819ce1ceaf7253f8419ddc1301c3
rishii99 <rishimadhesiya9984@gmail.com>
2025-12-04 23:31:16 +0530

    Refactor UniquePaths to Solution and add comments
    Renamed the main class from UniquePaths to Solution and added commented-out recursive solution for reference. Cleaned up main method and improved code organization for clarity.

--- Leetcode/UniquePaths.java
+++ Leetcode/UniquePaths.java
@@ -1,4 +1,25 @@
-class UniquePaths {
+// // Recurisive sol.
+// class UniquePaths {
+//     public int uniquePaths(int m, int n) {
+//         int[][] dp = new int[m][n];
+//         return solve(0, 0, m, n, dp);
+//     }
+//     private int solve(int i, int j, int m, int n, int[][] dp) {
+//         if (i == m - 1 && j == n - 1) return 1;
+//         if (i >= m || j >= n) return 0;
+//         if (dp[i][j] != 0) return dp[i][j];
+//         dp[i][j] = solve(i + 1, j, m, n, dp) + solve(i, j + 1, m, n, dp);
+//         return dp[i][j];
+//     }
+//     public static void main(String[] args) {
+//         UniquePaths obj = new UniquePaths();
+//         System.out.println(obj.uniquePaths(3, 7)); // example run
+//     }
+// }
+
+
+//Memoization sol.
+class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
         return solve(0, 0, m, n, dp);
@@ -9,9 +30,5 @@ class UniquePaths {
         if (dp[i][j] != 0) return dp[i][j];
         dp[i][j] = solve(i + 1, j, m, n, dp) + solve(i, j + 1, m, n, dp);
         return dp[i][j];
-    }
-    public static void main(String[] args) {
-        UniquePaths obj = new UniquePaths();
-        System.out.println(obj.uniquePaths(3, 7)); // example run
     }
 }
